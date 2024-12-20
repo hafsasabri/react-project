41cef9766ebae80ffbde6229cb653d8eb9c463f3
BarjaniYassir <161531441+BarjaniYassir@users.noreply.github.com>
2024-12-20 12:58:05 +0100

    Delete client directory

--- client/src/Redux/Constants/Cart.js
+++
@@ -1,8 +0,0 @@
-// Actions liées à la gestion des articles du panier
-export const ADD_ITEM_TO_CART = "ADD_ITEM_TO_CART"; // Ajouter un article au panier
-export const REMOVE_ITEM_FROM_CART = "REMOVE_ITEM_FROM_CART"; // Supprimer un article du panier
-export const CART_ITEM_CLEAR = "CART_ITEM_CLEAR"; // Vider le panier
-
-// Actions liées à l'enregistrement des informations de la commande
-export const CART_SAVE_SHIPPING_ADDRESS = "CART_SAVE_SHIPPING_ADDRESS"; // Sauvegarder l'adresse de livraison dans le panier
-export const SAVE_PAYMENT_METHOD = "SAVE_PAYMENT_METHOD"; // Sauvegarder le mode de paiement dans le panier
--- client/src/Redux/Constants/Product.js
+++
@@ -1,9 +0,0 @@
-// Actions pour la récupération de la liste des produits
-export const PRODUCT_LIST_REQ = "PRODUCT_LIST_REQ"; // Demande de récupération de la liste des produits
-export const PRODUCT_LIST_REQ_SUCCESS = "PRODUCT_LIST_REQ_SUCCESS"; // Succès de la récupération de la liste des produits
-export const PRODUCT_LIST_REQ_FAIL = "PRODUCT_LIST_REQ_FAIL"; // Échec de la récupération de la liste des produits
-
-// Actions pour la récupération des détails d'un produit
-export const PRODUCT_DETAIL_REQ = "PRODUCT_DETAIL_REQ"; // Demande de récupération des détails d'un produit
-export const PRODUCT_DETAIL_REQ_SUCCESS = "PRODUCT_DETAIL_REQ_SUCCESS"; // Succès de la récupération des détails d'un produit
-export const PRODUCT_DETAIL_REQ_FAIL = "PRODUCT_DETAIL_REQ_FAIL"; // Échec de la récupération des détails d'un produit
